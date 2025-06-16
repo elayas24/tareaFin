0508128620cf600cf101f34addc2d2200d0f0a48
elayas24 <jajalookoo1234@gmail.com>
2025-06-15 22:57:06 -0400

    actualicando

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,13 @@ function App() {
 
   return (
     
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/navbar" element={<Navbar />} />
-      <Route path="/contacto" element={<h1>CONTACTOS</h1>} />
-    </Routes>
+    <div className="grid grid-cols-4 grid-rows-4 gap-4 mx-96">
+                <div className="col-span-4 bg-amber-900 text-center font-bold underline text-6xl h-20">CINEMATECA BOLIVIANA</div>
+                <div className="col-span-4 row-start-2 h-20 w-[100%]"><Navbar/></div>
+                <div className="col-span-4 row-start-3 bg-amber-900" ><Cartilla/></div>
+                <div className="col-span-2 row-start-4 bg-amber-900 text-center pt-10">Elias Benjamin Vargas Valdivia</div>
+                <div className="col-span-2 col-start-3 row-start-4 bg-amber-900 text-center pt-10">Programacion Web 2</div>
+            </div>
 
 
     
